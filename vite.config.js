895dc5b8512309785a661783e5fdfcf00ac59843
QuatBaThien <86465894+QuatBaThien@users.noreply.github.com>
2023-06-22 10:11:03 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: './',
+  base: '/',
   build: {
     rollupOptions: {
       external: ['chillcafe.png']
