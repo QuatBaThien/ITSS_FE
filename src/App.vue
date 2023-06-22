09faba06f467cdd91c402b38efdb1eed75a82547
QuatBaThien <86465894+QuatBaThien@users.noreply.github.com>
2023-06-22 10:26:33 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -11,3 +11,18 @@ import Nav from './components/Nav.vue';
 
 </template>
 
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      this.counter++
+    }, 1000)
+  }
+}
+</script>
