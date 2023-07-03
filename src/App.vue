c73373e2fad3251226967436aa98e30209bc62ed
QuatBaThien <86465894+QuatBaThien@users.noreply.github.com>
2023-07-03 20:51:20 +0700

    [F] fix router

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import Nav from './components/Nav.vue';
 
 <template>
 
-    <Nav v-if="!$route.meta.hideNavbar" />
+    <Nav />
   <RouterView />
 
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,38 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../components/Home.vue'
-import Welcome from '../components/Welcome.vue'
 import Login from '../components/Login.vue'
 import Register from '../components/Register.vue'
 import CafeDetail from '../components/CafeDetail.vue'
-const routes =[
+import Welcome from '../components/Welcome.vue'
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
     {
       path: '/',
       name: 'home',
       component: Home
     },
-    {
+     {
       path: '/welcome',
       name: 'welcome',
-      component: Welcome,
-      meta: {
-        hideNavbar: true,
-       }
+      component: Welcome
     },
     {
       path: '/login',
       name: 'login',
-      component: Login,
-      meta: {
-        hideNavbar: true,
-       }
+      component: Login
     },
     {
       path: '/register',
       name: 'register',
-      component: Register,
-      meta: {
-        hideNavbar: true,
-       }
+      component: Register
     },
     {
       path: '/search/:keywords',
@@ -45,20 +38,7 @@ const routes =[
       component: CafeDetail
     },
   ]
-  const router = createRouter({
-    history: createWebHistory(),
-    routes
-  })
-  router.beforeEach((to, from, next) => {
-    const token = localStorage.getItem('token')
-  
-    if (to.meta.requiresAuth && !token) {
-      // User is not authenticated, redirect to login
-      next('/welcome')
-    } else {
-      next('/')
-    }
-  })
+})
 
 
 export default router;
